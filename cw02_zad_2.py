899708abd15a692c900338c055a95fbc2e695ae7
Milena Rozycka <milena.rozycka@uwr.edu.pl>
2025-11-04 12:49:42 +0100

    uzupelnione

--- cw02_zad_2.py
+++ cw02_zad_2.py
@@ -6,16 +6,20 @@ tresc = plik.read()
 print(tresc)
 
 x = int(tresc)
-#print(x)
+print(x)
 
 diff = x-wiek
+print(diff)
 
 if wiek < x:
-    zmienna = "Pierwszy student jest młodszy od studenta drugiego o " + str(diff) + " " +"lata."
+    zmienna = "Pierwszy student jest mlodszy od studenta drugiego o " + str(diff) + " " +"lat(a)."
     print(zmienna)
+else:
+    zmienna_2 = "Pierwszy student jest starszy od drugiego o " + str(abs(diff)) + " " + "lat(a)."
+    print(zmienna_2)
 
 
 # Zapis do pliku
 
 with open ("wiek_2.txt", 'a') as plik:
-    plik.write(zmienna)
+    plik.write(zmienna_2)
